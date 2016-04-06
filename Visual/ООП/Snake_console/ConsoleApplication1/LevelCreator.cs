3c9b46bd788ebc5831e72d245f15a9b272407bbb
Virtalik <v.golubev@mail.ru>
2016-04-06 10:58:22 +0300

    creating levelcreating class

---
+++ Visual/ООП/Snake_console/ConsoleApplication1/LevelCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class LevelCreator
+    {
+		Level lev;
+        LevelCreator(Level lev)
+		{
+			this.lev = lev;
+		}
+        
+
+    }
+	enum Level
+	{
+		Easy,
+		Middle,
+		Hard,
+		Insanity
+	}
+	
+}
